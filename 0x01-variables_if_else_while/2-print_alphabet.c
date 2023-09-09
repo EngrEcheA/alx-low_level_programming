4774e16ed1977ef65293c8a113df3aa7bcfa1076
EngrEcheA <auduechee@gmail.com>
2023-09-08 18:06:39 -0700

    print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - rogram that prints the alphabet
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	putchar(i);
+	putchar('\n');
+
+	return (0);
+}
