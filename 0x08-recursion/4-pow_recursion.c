86ea8c2f827f4318bfec47537645d35a827ae495
EngrEcheA <auduechee@gmail.com>
2023-09-28 10:11:42 -0700

    Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_resursion - returns the value of x raise to the power of y
+ * @x: value to raise
+ * @y: power
+ *
+ * Return: result of the power
+ */
+
+int _pow_resursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
